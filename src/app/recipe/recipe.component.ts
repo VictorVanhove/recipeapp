04e3f13c46f78373c871bce364c794e2be00c9d1
Pieter Van Der Helst <pieter@heyheyapps.com>
2019-01-19 12:13:52 +0100

    adding data to the component

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.name = 'spaghetti';
   }
 
+  ngOnInit() {}
 }
