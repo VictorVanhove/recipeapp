2c358799cc967ace80b0bcfede69e27aac43bf83
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-02-02 16:52:38 +0100

    recipe model class

---
+++ src/app/recipe.model.ts
@@ -0,0 +1,20 @@
+export class Recipe {
+  constructor(
+    private _name: string,
+    private _ingredients = new Array<string>(),
+    private _dateAdded = new Date()
+  ) {}
+
+  get name(): string {
+    return this._name;
+  }
+  get dateAdded(): Date {
+    return this._dateAdded;
+  }
+  get ingredients(): string[] {
+    return this._ingredients;
+  }
+  addIngredient(name: string, amount?: number, unit?: string) {
+    this._ingredients.push(`${amount || 1} ${unit || ''} ${name}`);
+  }
+}
